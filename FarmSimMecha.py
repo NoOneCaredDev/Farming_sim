896f1fcfc28d6c43d59710132011e5d333410a46
Cyynooo <medvedkoludmila24@gmail.com>
2024-09-18 21:54:24 +0300

    done
    Доделал работу программы, ДАЖЕ ошибок нет (вроде)

--- FarmSimMecha.py
+++ FarmSimMecha.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Interface:
     def __init__(self, animal: str, ready: bool, hunger: float, thirst: float):
         self.animal = animal
@@ -12,10 +15,10 @@ class Interface:
             print(f'Вы переполнили желудок. {self.animal} не успело в свой туалет..')
 
     def feed(self):
-        self.feed += 1
+        self.hunger += 1
         print('Вы накормили животное')
-        if self.feed >= 2:
-            print(f'Вы перекормили. {self.animal} не успело в свой туалет..')
+        if self.hunger >= 2:
+            print(f'Вы перекормили животное. {self.animal} не успело в свой туалет..')
 
     def special(self):
         if self.ready:
@@ -67,14 +70,19 @@ class Interface:
                 print(f"{any.animal} isn't ready to poop with eggs, please wait~!")
         print("-----AVAILABLE COMMANDS-----\n-Tip water\n-Tip food\n-Do special command")
 
-    def choose(self, choise: int):
-        match choise:
-            case 1:
-                self.drink()
-            case 2:
-                self.feed()
-            case 3:
-                self.special
+    def doing(self):
+        while True:
+            self.info()
+            choice = int(input(f'Выберите действие, которое хотите сделать над {self.animal} (water 1/food 2/special command 3): ' ))
+            match choice:
+                case 1:
+                    self.drink()
+                case 2:
+                    self.feed()
+                case 3:
+                    self.special()
+            if input('Хотите закончить с этим животным? yes/no ') == 'yes':
+                break
 
             
             
@@ -82,52 +90,48 @@ class Interface:
 
 a_cow = Interface('cow', False, 0.5, 0.7)
 a_sheep = Interface('sheep', True, 0.1, 0.9)
-a_dog = Interface('Dog', False, 0.4, 0.6)
+a_dog = Interface('dog', False, 0.4, 0.6)
 a_pig = Interface('pig', False, 0.2, 0.3)
 a_chicken = Interface('chicken', True, 0.8, 0.5)
-count = 0
 list = ['cow', 'sheep', 'dog', 'pig', 'chicken']
 choise = 0
+count = 0
 
 def day():
+    global count
+    count = count + 1
     while True:
-        animall = input('День N \nВыберите животное (cow, sheep, dog, pig, chicken): ')
-        if animall in list:
+        while True:
+            animall = input(f'День {count}, \nВыберите животное (cow, sheep, dog, pig, chicken): ')
+            if animall in list:
+                break
+            else:
+                print("Нет такого животного!!")
+        match animall:
+            case 'cow':
+                a_cow.doing()
+            case 'sheep':
+                a_sheep.doing()
+            case 'dog':
+                a_dog.doing()
+            case 'pig':
+                a_pig.doing()
+            case 'chicken':
+                a_chicken.doing()
+        if input('Вы хотите законичить сегодняшний день? yes/no ') == 'yes':
             break
-        else:
-            print("Нет такого животного!!")
-    match animall:
-        case 'cow':
-            a_cow.info()
-            choise = int(input('Выберите действие (1 drink, 2 feed, 3 special): '))
-            a_cow.choose(choise)
-        case 'sheep':
-            a_sheep.info()
-            choise = int(input('Выберите действие (1 drink, 2 feed, 3 special): '))
-            a_sheep.choose(choise)
-        case 'dog':
-            a_dog.info()
-            choise = int(input('Выберите действие (1 drink, 2 feed, 3 special): '))
-            a_dog.choose(choise)
-        case 'pig':
-            a_pig.info()
-            choise = int(input('Выберите действие (1 drink, 2 feed, 3 special): '))
-            a_pig.choose(choise)
-        case 'chicken':
-            a_chicken.info()
-            choise = int(input('Выберите действие (1 drink, 2 feed, 3 special): '))
-            a_chicken.choose(choise)
     for i in (a_chicken, a_cow, a_dog, a_pig, a_sheep):
         i.thirst = i.thirst -0.4
         i.hunger = i.thirst -0.4
+        if randint(0,1):
+            i.ready = True
 
-day()
 while True:
+    day()
     a = input('Играем дальше?  (yes/no)')
     if a == 'no':
+        for i in (a_chicken, a_cow, a_dog, a_pig, a_sheep):
+            i.info()
+            print('\n')
         break
-    if a == 'yes':
-        day()
-    
-
-#ААА Я НЕ УСПЕЛ. ОШИБКА. Может получится у тебя решить её..(
+print(f'Ферма прожила {count} дня(ей/)')
